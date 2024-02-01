e62815ffa5436c22079c427f1d13f7edb87a1cf0
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-02-01 02:06:59 -0500

    Edicion de Juego1

--- Juego1.h
+++ Juego1.h
@@ -14,19 +14,23 @@ public:
     Juego1()
     {
         setNombre("Adivina la palabra");
+        setNJugadores(2);
         nErrores = 0;
     }
 
     void establecerPalabra()
     {
         string palabra;
-        char rpta;
         bool confirmacion;
 
         do
         {
             cout << "Ingrese la palabra a adivinar: ";
             cin >> palabra;
+
+            confirmacion = confirmacion("palabra", this->palabra, palabra)
+
+            /*
             cout << "Su palabra es: " << palabra << " ? (S/N)";
             cin >> rpta;
 
@@ -41,13 +45,92 @@ public:
             }else{
                 cout<< "Caracter no reconocido";
             }
-            
-        } while (rpta != 'S');
+            */
+
+        } while (confirmacion);
     }
 
+    
+}
+
+void
+definirPista()
+{
+    string pista;
+    bool confirmacion;
+
+    do
+    {
+        cout << "Escriba una pista que permita adivinar la palabra: ";
+        cin >> pista;
+
+        confirmacion = confirmacion("palabra", this->palabra, palabra)
+    } while (confirmacion);
+
+    /*
+    cout << "Pista: " << pista << endl;
+
+    cout << "Su pista es: " << palabra << " ? (S/N)";
+    cin >> rpta;
+
+    if (rpta == 'S')
+    {
+        this->pista = pista;
+        cout << "Pista guardada exitósamente" << endl;
+        system("clear");
+    }
+    else if (rpta == 'N')
+    {
+        cout << "Ingrese nuevamente la información";
+    }
+    else
+    {
+        cout << "Caracter no reconocido";
+    }
+    */
+}
+
+void aumentarError()
+{
+    cout << "\nHa cometido un error" << endl;
+    nErrores++;
+    contabilizarErrores();
+}
+
 private:
-    string palabra;
-    int nErrores;
-};
+string palabra, pista;
+int nErrores;
+
+void contabilizarErrores()
+{
+    cout << "\nNúmero de errores: " << nErrores << endl;
+}
+
+bool confirmacion(string cuestion, string original, string verificando)
+    {
+        cout << "¿Su " << cuestion << " es: " << palabra << " ? (S/N)";
+        cin >> rpta;
+
+        if (rpta == 'S')
+        {
+            original = verificando;
+            cout << "Guardado exitoso" << endl;
+            system("clear");
+            return true;
+        }
+        else if (rpta == 'N')
+        {
+            cout << "Ingrese nuevamente la información";
+            return false;
+        }
+        else
+        {
+            cout << "Caracter no reconocido";
+            return false;
+        }
+    }
+
+}
+;
 
 #endif
