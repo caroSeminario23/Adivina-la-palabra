83c56359406c33f451c87c78de58e164dbe2b753
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-01-31 21:39:40 -0500

    Edición de plantilla Jugador

--- Jugador.h
+++ Jugador.h
@@ -1,13 +1,22 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 #ifndef JUGADOR_H
 #define JUGADOR_H
-
-#include <string>
 
 class Jugador {
     public:
         Jugador();
+        void definirNombre(){
+            cout<<"Ingrese su nombre: ";
+            cin>>nombre;
+            cout<<"Nombre: "<<nombre;
+        };
     private:
         string nombre;
+        int puntaje;
 
 };
 #endif
