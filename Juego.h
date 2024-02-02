b4ae373fd0c01de45102ecbe9f0e9475f0dcf321
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-02-02 00:20:02 -0500

    Edición de Juego y eliminación de Juego1

--- Juego.h
+++ Juego.h
@@ -4,23 +4,26 @@
 using namespace std;
 
 #ifndef JUEGO.H
-#define JUEGO.H
+#define JUEGO .H
 
-class Juego{
-    public:
-        Juego(){
-            nErrores=0;
-            cout<<"Juego creado"<<endl;
-        };
+class Juego
+{
+public:
+    Juego()
+    {
+        nErrores = 0;
+        cout << "Juego creado" << endl;
+    };
 
-        ~Juego(){
-            cout<<"Juego destruido"<<endl;
-        }
+    ~Juego()
+    {
+        cout << "Juego destruido" << endl;
+    }
 
     void establecerPalabra()
     {
         string palabra;
-        bool confirmacion;
+        bool verificacion;
 
         do
         {
@@ -28,17 +31,49 @@ class Juego{
             cin >> palabra;
             cout << "Palabra: " << palabra;
 
-            confirmacion = confirmacion("palabra", this->palabra, palabra);
+            verificacion = confirmacion("palabra", this->palabra, palabra);
         } while (confirmacion);
     }
 
-    private:
-        string palabra, pista;
-        int nErrores;
-        char *completando;
-    
+    void
+    definirPista()
+    {
+        string pista;
+        bool verificacion;
+
+        do
+        {
+            cout << "Escriba una pista que permita adivinar la palabra: ";
+            cin >> pista;
+
+            verificacion = confirmacion("pista", this->pista, pista);
+        } while (confirmacion);
+    }
+
+    void aumentarError()
+    {
+        cout << "\nHa cometido un error" << endl;
+        nErrores++;
+        contabilizarErrores();
+    }
+
+    void solicitarLetra(){
+        char letra;
+
+        cout<<"Escriba la letra que cree está contenida en la palabra: ";
+        cin>>letra;
+
+        buscarLetra(letra);
+    }
+
+private:
+    string palabra, pista;
+    int nErrores, *arregloPos;
+    char *completando;
+
     bool confirmacion(string cuestion, string original, string verificando)
     {
+        char rpta;
         cout << "¿Su " << cuestion << " es: " << palabra << " ? (S/N)";
         cin >> rpta;
 
@@ -60,8 +95,47 @@ class Juego{
             return false;
         }
     }
-    
 
+    void crearArregloDeVerificacion()
+    {
+        int nPalabras;
+        nPalabras = palabra.size();
+
+        completando = new char[nPalabras];
+
+        for(int i=0; i<nPalabras; i++){
+            completando[i]='-';
+        }
+    }
+
+    void crearArregloPosiciones(){
+        int nPalabras;
+
+        nPalabras = palabra.size();
+        arregloPos = new int[nPalabras];
+
+        for(int i=0; i<nPalabras; i++){
+            arregloPos[i]=i;
+        }
+    }
+
+    void contabilizarErrores()
+    {
+        cout << "\nNúmero de errores: " << nErrores << endl;
+    };
+
+    void buscarLetra(char letra)
+    {
+       for (int i=0; i<sizeof(arregloPos); i++){
+        if (palabra[arregloPos[i]]==letra){
+            completando[arregloPos[i]] = letra;
+        }
+       }
+    }
+
+    void reducirArregloPos(int tamaño){
+
+    }
 };
 
 #endif
